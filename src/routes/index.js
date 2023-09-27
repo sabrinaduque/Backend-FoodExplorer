38e3d8b86e04d0c795e27e990736b2661281af5e
sabrinaduque <sabrinanduque@hotmail.com>
2023-09-27 17:42:22 -0300

    implements ingredients controller and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const routes = Router()
 
 const usersRouter = require("./users.routes")
 const dishesRouter = require("./dishes.routes")
+const ingredientsRouter = require("./ingredients.routes")
 
 routes.use("/users", usersRouter)
 routes.use("/dishes", dishesRouter)
+routes.use("/ingredients", ingredientsRouter)
 
 module.exports = routes
