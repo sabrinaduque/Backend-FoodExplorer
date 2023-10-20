172c90b3359a447e718d747331362a511a58cab1
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-20 20:43:24 -0300

    update multer and authentication in dishes routes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,15 +1,22 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const DishesController = require("../controllers/DishesController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const ensureUserIsAdmin = require("../middlewares/ensureUserIsAdmin")
 
 const dishesRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const dishesController = new DishesController()
 
-dishesRoutes.get("/", dishesController.index)
-dishesRoutes.post("/", dishesController.create)
+dishesRoutes.use(ensureAuthenticated)
+
+dishesRoutes.get("/",  dishesController.index)
 dishesRoutes.get("/:id", dishesController.show)
-dishesRoutes.delete("/:id", dishesController.delete)
-dishesRoutes.put("/:id", dishesController.update)
+dishesRoutes.post("/", ensureUserIsAdmin, upload.single("image"), dishesController.create)
+dishesRoutes.delete("/:id", ensureUserIsAdmin, dishesController.delete)
+dishesRoutes.put("/:id", ensureUserIsAdmin, upload.single("image"), dishesController.update)
 
 module.exports = dishesRoutes
