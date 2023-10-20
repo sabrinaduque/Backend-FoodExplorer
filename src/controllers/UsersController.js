dcae5d66d9ba5bfbe545388d09b678183b55ccb2
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-20 20:39:34 -0300

    remove update user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -34,50 +34,8 @@ class UsersController {
       password: hashedPassword
     })
 
-    response.status(201).json()
+    return response.status(201).json()
   }
-
-  async update(request, response) {
-    const { name, email, password, old_password } = request.body
-    const { id } = request.params
-
-    const [user] = await knex("users").where({ id })
-
-    if (!user) {
-      throw new AppError("Usuário não encontrado")
-    }
-
-    user.name = name ?? user.name
-    user.email = email ?? user.email
-
-    const [userWithUpdateEmail] = await knex("users").where({ email })
-
-    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
-      throw new AppError("Este e-mail já está em uso.")
-    }
-
-    if (password && old_password) {
-      const checkOldPassword = await compare(old_password, user.password)
-
-      if (!checkOldPassword) {
-        throw new AppError("A senha antiga não confere.")
-      }
-
-      user.password = await hash(password, 8)
-    }
-
-    await knex("users")
-      .where({ id: id })
-      .update({
-        name: user.name,
-        email: user.email,
-        password: user.password,
-        updated_at: knex.fn.now(),
-      })
-
-    return response.json()
-  }
-
 }
 
 module.exports = UsersController
