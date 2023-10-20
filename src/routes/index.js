33d31412e9cc78360c5d00d3446a5a3bd63cebdf
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-20 20:44:10 -0300

    implements sessions router in index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const routes = Router()
 const usersRouter = require("./users.routes")
 const dishesRouter = require("./dishes.routes")
 const ingredientsRouter = require("./ingredients.routes")
+const sessionsRouter = require("./sessions.routes")
 
 routes.use("/users", usersRouter)
 routes.use("/dishes", dishesRouter)
+routes.use("/sessions", sessionsRouter)
 routes.use("/ingredients", ingredientsRouter)
 
 module.exports = routes
