8aa5a40bb010fd7075b1efb863f961dd318d2d60
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-20 20:27:58 -0300

    implements cors

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 require("express-async-errors")
+const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 const express = require("express")
-
-const AppError = require("./utils/AppError")
-
 const routes = require("./routes")
+const cors = require("cors")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
-      message: error.message
+      message: error.message,
     })
   }
 
@@ -22,10 +25,9 @@ app.use((error, request, response, next) => {
 
   return response.status(500).json({
     status: "error",
-    message: "Internal server error"
+    message: "Internal server error",
   })
 })
 
-const PORT = 3333;
+const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
-
