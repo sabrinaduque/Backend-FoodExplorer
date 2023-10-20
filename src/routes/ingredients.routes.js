782831563bdf2b236e858c8b126dea5980e46113
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-20 20:45:22 -0300

    implements authentication in ingredients routes

--- src/routes/ingredients.routes.js
+++ src/routes/ingredients.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express")
 
 const IngredientsController = require("../controllers/IngredientsController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const ingredientsController = new IngredientsController()
 
 const ingredientsRoutes = Router()
 
 
-ingredientsRoutes.get("/:dish_id", ingredientsController.index)
+ingredientsRoutes.get("/:dish_id", ensureAuthenticated, ingredientsController.index)
 
 module.exports = ingredientsRoutes
